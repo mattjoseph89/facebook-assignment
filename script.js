6770ffa6790d3a708705c2e0b64fc1a0ca568395
Matt Joseph Wurth <mattjwurth@gmail.com>
2017-08-18 09:22:42 -0500

    replies will show/unshow

--- script.js
+++ script.js
@@ -34,14 +34,79 @@ window.addEventListener("load", function(){
 
 // display comment replies
 	
-	
+	var display_reply = document.getElementsByClassName("comment__info");
+    
+	for (var i=0;i < display_reply.length; i++)  {
 
+  		display_reply[i].childNodes[3].addEventListener("click", function(){
+  			var replies= event.target.parentNode.parentNode.childNodes[5];
+  			if (replies.style.display == "none") {
+  				replies.style.display = "block"
+  			}
+  			else {
+  				replies.style.display = "none"
+  			}
+	}); 			
+}			 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+var reply_likes = document.getElementsByClassName("comment__info");
+
+	for (var i=0;i < display_reply.length; i++){
+
+		reply_likes[i].childNodes[1].addEventListener("click", function(){
+
+		if  (reply_likes[i].childNodes[1].innerHTML==="Like"){
+			reply_likes[i].childNodes[1].innerHTML="Unlike"
+			var reply_like_plus = event.target.parentNode.childNodes[5].textContent;
+	    	newlikes=parseInt(reply_like_plus)+1;
+	    	event.target.parentNode.childNodes[5].innerHTML = newlikes + " likes";
+			}
+		else 
+			{reply_likes[i].childNodes[1].innerHTML="Like"
+			var like_minus = document.getElementsByClassName("likeability")[0].textContent;
+	    	newlikes=parseInt(like_minus)-1;
+	    	document.getElementsByClassName("likeability")[0].innerHTML = newlikes + " likes";
+			}
+		});
+
+	}
 
 
 
 });
-
-
-
 
 
